ed3003ace8f1948a9491f91675d0b1724a1dd996
Eduardo Sanchez <91806621+Layloe@users.noreply.github.com>
2022-06-22 17:44:21 -0700

    Added connection to Mongo

--- server.js
+++ server.js
@@ -3,3 +3,20 @@ const app = express()
 const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
+const PORT = 8000
+
+let db,
+dbConnectionString = process.env.DB_STRING,
+dbName = 'sample_mflix',
+collection 
+
+MongoClient.connect(dbConnectionString)
+.then(client => {
+    console.log(`Connected to Database`)
+    db = client.db(dbName)
+    collection = db.collection('movies')
+})
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server is running on port`)
+})
